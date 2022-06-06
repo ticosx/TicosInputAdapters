859cceac09e82b06e9b2e5e81d2346acc5139e0b
Sawyer <sawyer@SawyerdeMacBook-Pro.local>
2022-06-07 01:16:15 +0800

    feat: 移植参考驱动

--- src/TkmTpFt6336.cpp
+++ src/TkmTpFt6336.cpp
@@ -1,38 +1,146 @@
 #include "TkmTpFt6336.h"
+#include "HwHelper.h"
 
-TkmTpFt6336* TkmTpFt6336::instance = NULL;
+TkmTpFt6336 *TkmTpFt6336::instance = NULL;
+static const uint8_t FT5206_TPX_TBL[5] = {FT6336G_TP1_REG, FT6336G_TP2_REG, FT6336G_TP3_REG, FT6336G_TP4_REG, FT6336G_TP5_REG};
 
-TkmTpFt6336::TkmTpFt6336(Adafruit_I2CDevice * i2c) : LvglInputAdapter(i2c) {
+TkmTpFt6336::TkmTpFt6336(TwoWire* i2c, uint8_t rstPin, uint16_t width, uint16_t height)
+    : LvglInputAdapter(i2c, FT6336G_ADDR), rstPin(rstPin), width(width), height(height)
+{
   // Release the origin instance
-  if(instance!=NULL){
-    delete(instance);
+  if (instance != NULL)
+  {
+    delete (instance);
   }
   input_cb = TkmTpFt6336::read;
   instance = this;
+  init();
 }
 
-TkmTpFt6336::~TkmTpFt6336(){
+TkmTpFt6336::~TkmTpFt6336()
+{
+  instance = NULL;
 }
 
-void TkmTpFt6336::init(){
-
+void TkmTpFt6336::reset()
+{
+  digitalWrite(rstPin, 0);
+  delay(20);
+  digitalWrite(rstPin, 1);
 }
 
-void TkmTpFt6336::read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data){
-    uint16_t touchX, touchY;
+bool TkmTpFt6336::init()
+{
+  // reset();
+  delay(50);
 
-    bool touched = true;// TODO: Get touch info from device
+  uint8_t reg = 0;
+  //正常操作模式
+  I2C_WRITE_REG(FT6336G_DEVIDE_MODE, reg);
+  //查询模式
+  I2C_WRITE_REG(FT6336G_ID_G_MODE, reg);
+  //触摸有效值，越小越灵敏
+  reg = 22;
+  I2C_WRITE_REG(FT6336G_ID_G_THGROUP, reg);
+  //激活周期
+  reg = 12;
+  I2C_WRITE_REG(FT6336G_ID_G_PERIODACTIVE, reg);
+  uint8_t addr = FT6336G_ID_G_LIB_VERSION;
+  uint8_t data[2];
+  //读取版本号
+  if(i2cdevice->write_then_read(&addr, sizeof(addr), data, 2, true)){
+    logDebug("TP ID:%x\r\n", ((uint16_t)data[0] << 8) + data[1]);
+  } else {
+    logDebug("Read TP I2C wrong\r\n");
+    return false;
+  }
+  if ((data[0] == 0X30 && data[1] == 0X03) || data[1] == 0X01 || data[1] == 0X02)
+  {
+    logInfo("TP ID:%x\r\n", ((uint16_t)data[0] << 8) + data[1]);
+    return false;
+  }
+  return true;
+}
 
-    if( !touched )
+void TkmTpFt6336::read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data){
+  if(getInstance()!=NULL){
+    getInstance()->_read(indev_driver, data);
+  }
+}
+
+void TkmTpFt6336::_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+
+  uint8_t buf[4];
+  uint8_t res = 0;
+  uint8_t temp;
+  uint8_t x[6], y[6];
+  static uint8_t filter = 0;
+  static uint8_t mode = 0;
+  filter++;
+  //空闲时降低读取频率
+  if ((filter++ % READ_INTERVAL) == 0 || filter < READ_INTERVAL)
+  {
+    //读取触摸点的状态
+    mode = I2C_READ_REG(FT6336G_REG_NUM_FINGER);
+    if(filter%100 == 0)
+      logDebug("TP Mode :%x\r\n", mode);
+    //读到些结果
+    if ((mode & 0XF) && ((mode & 0XF) < 6))
     {
-        data->state = LV_INDEV_STATE_REL;
+      //将点的个数转换为1的位数
+      temp = 0XFF << (mode & 0XF);
+      temp = ~temp;
+      for (uint8_t i = 0; i < 5; i++)
+      {
+        //该点按下
+        if (temp & (1 << i))
+        {
+          //读取该点坐标
+          i2cdevice->write_then_read(&FT5206_TPX_TBL[i], sizeof(FT5206_TPX_TBL[i]), buf, 4, true);
+          if (width >= height)
+          {
+            y[i] = ((uint16_t)(buf[0] & 0X0F) << 8) + buf[1];
+            x[i] = ((uint16_t)(buf[2] & 0X0F) << 8) + buf[3];
+          }
+          else
+          {
+            x[i] = width - (((uint16_t)(buf[0] & 0X0F) << 8) + buf[1]);
+            y[i] = ((uint16_t)(buf[2] & 0X0F) << 8) + buf[3];
+          }
+          if ((buf[0] & 0XF0) != 0X80)
+          {
+            //不是contact事件，无效
+            x[i] = y[i] = 0;
+          }
+        }
+      }
+      res = 1;
+      if (x[0] == 0 && y[0] == 0)
+      {
+        //无效数据
+        mode = 0;
+      }
+      //重置扫描标志
+      filter = 0;
     }
-    else
-    {
-        data->state = LV_INDEV_STATE_PR;
+  }
+  if ((mode & 0X3F) == 0)
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PR;
 
-        /*Set the coordinates*/
-        data->point.x = touchX;
-        data->point.y = touchY;
-    }
+    /*Set the coordinates*/
+    data->point.x = x[0];
+      logDebug("x :%x\r\n", x[0]);
+    data->point.y = y[0];
+      logDebug("y :%x\r\n", y[0]);
+  }
+  if (filter >= 210)
+  {
+    filter = 10;
+  }
 }
--- src/TkmTpFt6336.h
+++ src/TkmTpFt6336.h
@@ -4,22 +4,46 @@
 #include <Arduino.h>
 #include "lvgl.h"
 #include "LvglInputAdapter.h"
+#include "Log.h"
+#include <Wire.h>
+
+#define FT6336G_ADDR  0x38
+
+#define FT6336G_DEVIDE_MODE 0x00
+#define FT6336G_REG_NUM_FINGER  0x02
+#define FT6336G_TP1_REG         0X03
+#define FT6336G_TP2_REG         0X09
+#define FT6336G_TP3_REG         0X0F
+#define FT6336G_TP4_REG         0X15
+#define FT6336G_TP5_REG         0X1B
+#define	FT6336G_ID_G_LIB_VERSION  0xA1
+#define FT6336G_ID_G_MODE       0xA4
+#define FT6336G_ID_G_THGROUP    0x80
+#define FT6336G_ID_G_PERIODACTIVE 0x88
+
+#define READ_INTERVAL   10 
 
 /*!
- * @brief The XRenderer of 16x16 LED screen
+ * @brief FT6336G 触摸屏
  */
 class TkmTpFt6336 : public LvglInputAdapter {
 
   public:
-    TkmTpFt6336(Adafruit_I2CDevice *);
+    TkmTpFt6336(TwoWire *i2c, uint8_t rstPin, uint16_t width, uint16_t height);
     virtual ~TkmTpFt6336();
     static TkmTpFt6336* getInstance() {return instance;};
+    bool init();
+    void reset();
 
   protected:
     static TkmTpFt6336* instance;
     static void read(lv_indev_drv_t *, lv_indev_data_t *);
 
+    uint8_t rstPin;
+    uint16_t width;
+    uint16_t height;
+
   private:
-    void init();
+    void _read(lv_indev_drv_t *, lv_indev_data_t *);
 };
 #endif // TKM_TP_FT6336_h
